02634efa64a102a04d25d9d0f0a13f1590248345
Hollar <133783172+H0llar@users.noreply.github.com>
2024-08-10 22:31:24 +0900

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,10 +7,16 @@ from strings import (
     NO_ORDER_NUMBER_PROVIDED_MESSAGE,
     ORDER_INFO_TEMPLATE,
 )
+from datetime import datetime
+import aiohttp
+import asyncio
 import requests
 import base64
 import config
+import re
+import locale
 
+locale.setlocale(locale.LC_TIME, 'Russian')
 
 def get_auth_header():
     auth_str = f"{config.AUTH_USERNAME}:{config.AUTH_PASSWORD}"
@@ -19,25 +25,45 @@ def get_auth_header():
     return {"Authorization": f"Basic {auth_base64}"}
 
 
+async def fetch(session, url, headers):
+    async with session.get(url, headers=headers) as response:
+        return await response.json()
+
+
+def format_date(date_str):
+    date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S')
+    formatted_date = date_obj.strftime('%d %B %Y, %H:%M')
+    return formatted_date
+
+
+def format_status(status):
+    words = re.findall(r'[A-ZА-Я][^A-ZА-Я]*', status)
+    formatted_status = ' '.join(word.capitalize() for word in words)
+    return formatted_status
+
+
 async def start(update: Update, context: CallbackContext):
     await update.message.reply_text(START_MESSAGE)
 
 
 async def get_orders(update: Update, context: CallbackContext):
     telegram_id = update.message.from_user.username
-    url = f'{config.API_BASE_URL}/@{telegram_id}//orders?telegram_id=@{telegram_id}'
+    url = f'{config.API_BASE_URL}/@{telegram_id}/orders?telegram_id=@{telegram_id}'
     headers = get_auth_header()
-    response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        orders = response.json()
+    async with aiohttp.ClientSession() as session:
+        orders = await fetch(session, url, headers)
+
+
+    if orders:
         for order in orders:
+            formatted_status = format_status(order['status'])
             await update.message.reply_text(
                 ORDER_INFO_TEMPLATE.format(
                     number=order['number'],
                     product=order['product'],
-                    status=order['status'],
-                    date=order['date'],
+                    status=formatted_status,
+                    date=format_date(order['date']),
                     partner=order['partner'],
                     sum=order['sum'],
                     telegram_id=order['telegram_id']
@@ -46,23 +72,25 @@ async def get_orders(update: Update, context: CallbackContext):
     else:
         await update.message.reply_text(NO_ORDERS_FOUND_MESSAGE)
 
+
 async def get_order_by_number(update: Update, context: CallbackContext):
     telegram_id = update.message.from_user.username
     order_number = context.args[0]
-
     url = f'{config.API_BASE_URL}/@{telegram_id}/orders/{order_number}?telegram_id=@{telegram_id}'
     headers = get_auth_header()
 
-    response = requests.get(url, headers=headers)
+    async with aiohttp.ClientSession() as session:
+        orders = await fetch(session, url, headers)
 
-    if response.status_code == 200:
-        order = response.json()
+
+    if order:
+        formatted_status = format_status(order['status'])
         await update.message.reply_text(
             ORDER_INFO_TEMPLATE.format(
                 number=order['number'],
                 product=order['product'],
                 status=order['status'],
-                date=order['date'],
+                date=format_date(order['date']),
                 partner=order['partner'],
                 sum=order['sum'],
                 telegram_id=order['telegram_id']
